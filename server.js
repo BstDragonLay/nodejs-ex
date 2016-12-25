64d85110b3bb779046668eb3dce467459a9f815f
BstDragonLay <efrainlaycastillo@hotmail.com>
2016-12-25 00:24:24 -0500

    server #2

--- server.js
+++ server.js
@@ -72,10 +72,10 @@ var db = null,
     dbDetails = new Object();
 
 var initDb = function(callback) {
-  if (mongoURL == null) return;
+/*  if (mongoURL == null) return;
 
   var mongodb = require('mongodb');
-  if (mongodb == null) return;
+  if (mongodb == null) return;*/
 
   mongoose.connect(mongoURL, function(err, conn) {
     if (err) {
