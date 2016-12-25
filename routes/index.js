60127ef4b92662c51e49ec2f436c41a274500436
BstDragonLay <efrainlaycastillo@hotmail.com>
2016-12-24 20:44:46 -0500

    mongodb register - getdata erro#3

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next){
   res.render('index', {title: 'APP'});
 });
 //register
-router.get('/register', function(req, res, next){
+router.post('/register', function(req, res, next){
   var items = {
     name: req.body.name,
     password: req.body.password
