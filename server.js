c7e6831ed75e737481b4af8a25ccc40e51bd0329
BstDragonLay <efrainlaycastillo@hotmail.com>
2016-12-30 01:40:26 -0500

    mongodb issues#3

--- server.js
+++ server.js
@@ -190,9 +190,9 @@ app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500).send('Something bad happened!');
 });
-initDb(function(err){
+/*initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
-});
+});*/
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
